081514a1ad332d83962701934bbcfe8539b2656e
Long <long@Longs-MacBook-Pro.local>
2025-04-30 22:12:26 -0400

    ???

--- src/core/assets.py
+++ src/core/assets.py
@@ -2,16 +2,15 @@ import pygame
 
 from utilities.sprite import slice_sheet
 
-
 # Load sprites (png, webp or jpg for web compatibility)
-ICON = pygame.image.load("assets/icon.png")
-DEBUG_SPRITE = pygame.image.load("assets/img/dvd_logo.png")
-DEBUG_FRAMES = slice_sheet("assets/img/impossible_spin.png", 64, 64)
+ICON = pygame.image.load("src/assets/icon.png")
+DEBUG_SPRITE = pygame.image.load("src/assets/img/dvd_logo.png")
+DEBUG_FRAMES = slice_sheet("src/assets/img/impossible_spin.png", 64, 64)
 
 # Load audio (ogg for web compatibility)
-DEBUG_THEME = pygame.mixer.music.load("assets/sfx/theme.ogg")
+DEBUG_THEME = pygame.mixer.music.load("src/assets/sfx/theme.ogg")
 
 # Load fonts (ttf for web compatibility)
-DEBUG_FONT = pygame.font.Font("assets/joystix.ttf", 10)
+DEBUG_FONT = pygame.font.Font("src/assets/joystix.ttf", 10)
 
 print("Loaded assets")
